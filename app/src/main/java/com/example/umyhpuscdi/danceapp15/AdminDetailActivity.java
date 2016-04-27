f234ad176c744dda532388b47218670e31ec1071
unknown <umyharumdh@5HK-YH-8560W09.fuedu.info>
2016-04-27 15:37:52 +0200

    Tabview Fragment from Dhana

---
+++ app/src/main/java/com/example/umyhpuscdi/danceapp15/AdminDetailActivity.java
@@ -0,0 +1,84 @@
+package com.example.umyhpuscdi.danceapp15;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
+import java.util.Vector;
+
+/**
+ * Created by umyharumdh on 2016-04-27.
+ */
+public class AdminDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_detail);
+
+        //Adding Fragment java class here...
+        List<Fragment> fragmentlist_tabview = new Vector<>();
+        fragmentlist_tabview.add(Fragment.instantiate(this,Participant_TabView.class.getName()));
+        fragmentlist_tabview.add(Fragment.instantiate(this,Calender_TabView.class.getName()));
+        fragmentlist_tabview.add(Fragment.instantiate(this,Info_TabView.class.getName()));
+
+        PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager(),fragmentlist_tabview);
+        final ViewPager viewPager = (ViewPager)findViewById(R.id.admin_detail_viewpager);
+        viewPager.setAdapter(pageAdapter);
+
+        // starts ActionBar Tablistener
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(actionBar.NAVIGATION_MODE_TABS);
+
+        final ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+            @Override
+            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+            }
+
+            @Override
+            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+            }
+        };
+        // ends ActionBar Tablistener
+
+                // setting text to view in Tab
+             actionBar.addTab(actionBar.newTab().setText("Deltagare").setTabListener(tabListener));
+             actionBar.addTab(actionBar.newTab().setText("Kalender").setTabListener(tabListener));
+             actionBar.addTab(actionBar.newTab().setText("Info").setTabListener(tabListener));
+
+        // starts ViewPager changeListener here....
+
+             viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                 @Override
+                 public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                 }
+
+                 @Override
+                 public void onPageSelected(int position) {
+                     actionBar.setSelectedNavigationItem(position);
+
+                 }
+
+                 @Override
+                 public void onPageScrollStateChanged(int state) {
+
+                 }
+             });
+        // ends ViewPager changeListener here....
+
+    } // ends OnCreate
+} //ends AdminDetailActivity
